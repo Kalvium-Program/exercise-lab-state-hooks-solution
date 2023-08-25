f9a1dd4bab00b84fb720cccce67d59cadcecb765
Kalvium <109285740+Kalvium-Program@users.noreply.github.com>
2023-08-25 15:32:36 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,31 @@
+![Logo](https://s3.ap-south-1.amazonaws.com/kalvi-education.github.io/front-end-web-development/Kalvium-Logo.png)
+
+### Learning Goals:
+The aim of this exercise is to practice and understand the concept of `state's` using `functional-component`.
+For this exercise, we will be using 2 `hooks` - 
+
+1. useState()
+2. useEffect()
+
+And by using the above two hooks, we will try to achieve the same functionalities that we achieved using `component life cycle exercise` and `state management using class-component exercise`. 
+
+### Steps to follow:
+
+1. Clone this repository.
+
+2. On your terminal go inside `exercise-lab-state-hooks-boilerplate` directory and use the following command: `npm install`.This command install's all the packages to your system that this project is using.
+Here is a link to get a better understanding of what and how exactly the above command works.
+
+[What is npm install](https://www.stackchief.com/tutorials/npm%20install%20%7C%20how%20it%20works)
+
+3. Once everything is ready, you can type `npm run dev` -> inorder to run the application. You should be able to see a server running and see the output as shown below:
+
+![props](https://s3.ap-south-1.amazonaws.com/kalvi-education.github.io/front-end-web-development/props-exercise-react.png)
+
+
+### Task:
+
+Your task is to use hooks (`useState`,`useEffect`) in order to achieve the following output: 
+
+![hooks](https://s3.ap-south-1.amazonaws.com/kalvi-education.github.io/front-end-web-development/exercise-hooks.gif)
+
---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import Data from "./components/Data"
+import Image from "./components/Image"
+
+function App() {
+  let localDate = new Date()
+  localDate = localDate.toLocaleTimeString()
+
+  const [currDate, setDate] = useState(localDate);
+  const [displayNewText, setDisplay] = useState(true);
+  const [newText, setText] = useState("New Text Added")
+
+  useEffect(()=>{
+    setText(displayNewText?"New Text Added":null)
+  }
+    ,[displayNewText])
+
+  console.log(displayNewText)
+
+  return (
+    <div>
+    <header>
+      <img src="https://s3.ap-south-1.amazonaws.com/kalvi-education.github.io/front-end-web-development/Kalvium-Logo.png" alt=""></img>
+      <h3>{currDate}</h3>
+
+      <h2>{newText}</h2>
+      <button onClick={()=>{setDisplay(false)}}>Remove New Text</button>
+      <button onClick={()=>{setDisplay(true)}}>Add New Text</button>
+
+      <h3>Kalvium Education</h3>
+      
+    </header>
+    <div>
+      <p>It is a long established fact that a reader will be distracted by the readable content of a 
+        page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less 
+        normal distribution of letters, as opposed to using 'Content here, content here', making it look 
+        like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum 
+        as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in 
+        their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on 
+        purpose (injected humour and the like).
+      </p>
+    </div>
+    <Image data={Data}></Image>
+  </div>
+  );
+}
+
+export default App;
